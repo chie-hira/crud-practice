56c1d22e07bb3c086ce4c6087d17a1e83d6266b1
chie-hira <capella.a36@gmail.com>
2023-11-26 22:39:42 +0900

    ResultMapでuser_nameとmaterial_nameも取得

--- src/main/java/com/raisetech/servicepractice/entity/StoragePlace.java
+++ src/main/java/com/raisetech/servicepractice/entity/StoragePlace.java
@@ -5,15 +5,20 @@ import java.time.LocalDate;
 public class StoragePlace {
     private int id;
     private LocalDate storage_date;
+    private String storage_place;
     private int user_id;
+    private String user_name;
     private int material_id;
-    private User user;
+    private String material_name;
 
-    public StoragePlace(int id, LocalDate storage_date, int user_id, int material_id) {
+    public StoragePlace(int id, LocalDate storage_date, String storage_place, int user_id, int material_id, String user_name, String material_name) {
         this.id = id;
         this.storage_date = storage_date;
+        this.storage_place = storage_place;
         this.user_id = user_id;
+        this.user_name = user_name;
         this.material_id = material_id;
+        this.material_name = material_name;
     }
 
     public int getId() {
@@ -24,11 +29,23 @@ public class StoragePlace {
         return storage_date;
     }
 
+    public String getStorage_place() {
+        return storage_place;
+    }
+
     public int getUser_id() {
         return user_id;
     }
 
+    public String getUser_name() {
+        return user_name;
+    }
+
     public int getMaterial_id() {
         return material_id;
+    }
+
+    public String getMaterial_name() {
+        return material_name;
     }
 }
--- src/main/java/com/raisetech/servicepractice/mapper/StoragePlaceMapper.java
+++ src/main/java/com/raisetech/servicepractice/mapper/StoragePlaceMapper.java
@@ -2,13 +2,27 @@ package com.raisetech.servicepractice.mapper;
 
 import com.raisetech.servicepractice.entity.StoragePlace;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface StoragePlaceMapper {
-    @Select("SELECT * FROM storage_places")
+//    @Select("SELECT * FROM storage_places")
+//    List<StoragePlace> all();
+
+    @Select("SELECT storage_places.*, users.name as user_name, materials.material_name as material_name FROM storage_places JOIN users ON storage_places.user_id = users.id JOIN materials ON storage_places.material_id = materials.id")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "storage_date", column = "storage_date"),
+            @Result(property = "storage_place", column = "storage_place"),
+            @Result(property = "user_id", column = "user_id"),
+            @Result(property = "user_name", column = "user_name"),
+            @Result(property = "material_id", column = "material_id"),
+            @Result(property = "material_name", column = "material_name")
+    })
     List<StoragePlace> all();
 
 }

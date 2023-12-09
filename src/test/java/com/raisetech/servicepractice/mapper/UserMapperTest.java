15016e77e2d7e7dd202c9dcc63023b5ba43ea33b
chie-hira <capella.a36@gmail.com>
2023-12-09 21:59:56 +0900

    UserMapperを単体テスト

--- src/test/java/com/raisetech/servicepractice/mapper/UserMapperTest.java
+++ src/test/java/com/raisetech/servicepractice/mapper/UserMapperTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -20,7 +21,7 @@ public class UserMapperTest {
 
     @Test
     @Sql(
-            scripts = {"classpath:/sqlannotation/delete-users.sql", "classpath:/sqlannotation/insert-users.sql"},
+            scripts = {"classpath:/sqlannotation/delete-users.sql","classpath:/sqlannotation/insert-users.sql"},
             executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
     )
     @Transactional
@@ -32,6 +33,42 @@ public class UserMapperTest {
                         new User(1,"megu", "megu@neko.com"),
                         new User(2,"deji", "deji@neko.com"),
                         new User(3,"mopu", "mopu@neko.com")
+                );
+    }
+    @Test
+    void idからユーザーを取得できること() {
+        int userId = 1;
+        Optional<User> user = userMapper.findById(userId);
+        assertThat(user)
+                .isPresent()
+                .hasValueSatisfying(u -> assertThat(u)
+                        .isEqualTo(new User(userId, "megu", "megu@neko.com")));
+    }
+
+    @Test
+    void emailからユーザーを取得できること() {
+        String email = "deji@neko.com";
+        Optional<User> user = userMapper.findByEmail(email);
+        assertThat(user)
+                .isPresent()
+                .hasValueSatisfying(u -> assertThat(u)
+                        .isEqualTo(new User(2, "deji", email)));
+    }
+
+    @Test
+    void ユーザーを登録できること() {
+        User user = new User("kuro", "kuro@neko.com");
+        userMapper.insert(user);
+        int userId = user.getId();
+
+        List<User> allUsers = userMapper.findAll();
+        assertThat(allUsers)
+                .hasSize(4)
+                .contains(
+                        new User(1,"megu", "megu@neko.com"),
+                        new User(2,"deji", "deji@neko.com"),
+                        new User(3,"mopu", "mopu@neko.com"),
+                        new User(userId,"kuro", "kuro@neko.com")
                 );
     }
 }
